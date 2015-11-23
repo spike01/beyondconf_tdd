be99eaaaafc8406783341ad108f2f37bfa143057
Spike <spike01@gmail.com>
2015-11-23 20:28:31 +0000

    REFACTOR - generalise is_divisible_by?

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,26 +1,18 @@
 class Fizzbuzz
 
   def play(number)
-    if is_divisible_by_15?(number)
+    if is_divisible_by?(15, number)
       "Fizzbuzz"
-    elsif is_divisible_by_5?(number)
+    elsif is_divisible_by?(5, number)
       "Buzz"
-    elsif is_divisible_by_3?(number)
+    elsif is_divisible_by?(3, number)
       "Fizz"
     else
       number
     end
   end
 
-  def is_divisible_by_15?(number)
-    number % 15 == 0
-  end
-
-  def is_divisible_by_5?(number)
-    number % 5 == 0
-  end
-
-  def is_divisible_by_3?(number)
-    number % 3 == 0
+  def is_divisible_by?(divisor, number)
+    number % divisor == 0
   end
 end
