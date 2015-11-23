e912cb693e8e849bf76aa9f34564a063dc1f7973
Spike <spike01@gmail.com>
2015-11-23 20:31:05 +0000

    OPTIONAL REFACTOR - extract arrange into subject

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,44 +2,39 @@ require "fizzbuzz"
 
 RSpec.describe Fizzbuzz do
 
+  subject(:fizzbuzz) { Fizzbuzz.new }
+
   it "fizzes for 3" do
-    fizzbuzz = Fizzbuzz.new
     result = fizzbuzz.play(3)
     expect(result).to eq("Fizz")
   end
 
   it "fizzes for 6" do
-    fizzbuzz = Fizzbuzz.new
     result = fizzbuzz.play(6)
     expect(result).to eq("Fizz")
   end
 
   it "buzzes for 5" do
-    fizzbuzz = Fizzbuzz.new
     result = fizzbuzz.play(5)
     expect(result).to eq("Buzz")
   end
 
   it "buzzes for 10" do
-    fizzbuzz = Fizzbuzz.new
     result = fizzbuzz.play(10)
     expect(result).to eq("Buzz")
   end
 
   it "fizzbuzzes for 15" do
-    fizzbuzz = Fizzbuzz.new
     result = fizzbuzz.play(15)
     expect(result).to eq("Fizzbuzz")
   end
 
   it "fizzbuzzes for 30" do
-    fizzbuzz = Fizzbuzz.new
     result = fizzbuzz.play(30)
     expect(result).to eq("Fizzbuzz")
   end
 
   it "returns the number otherwise" do
-    fizzbuzz = Fizzbuzz.new
     result = fizzbuzz.play(1)
     expect(result).to eq(1)
   end
