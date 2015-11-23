8f126265298d050db9df813ede29c7876bc72df2
Spike <spike01@gmail.com>
2015-11-23 20:32:44 +0000

    OPTIONAL REFACTOR - inline result

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,37 +5,30 @@ RSpec.describe Fizzbuzz do
   subject(:fizzbuzz) { Fizzbuzz.new }
 
   it "fizzes for 3" do
-    result = fizzbuzz.play(3)
-    expect(result).to eq("Fizz")
+    expect(fizzbuzz.play(3)).to eq("Fizz")
   end
 
   it "fizzes for 6" do
-    result = fizzbuzz.play(6)
-    expect(result).to eq("Fizz")
+    expect(fizzbuzz.play(6)).to eq("Fizz")
   end
 
   it "buzzes for 5" do
-    result = fizzbuzz.play(5)
-    expect(result).to eq("Buzz")
+    expect(fizzbuzz.play(5)).to eq("Buzz")
   end
 
   it "buzzes for 10" do
-    result = fizzbuzz.play(10)
-    expect(result).to eq("Buzz")
+    expect(fizzbuzz.play(10)).to eq("Buzz")
   end
 
   it "fizzbuzzes for 15" do
-    result = fizzbuzz.play(15)
-    expect(result).to eq("Fizzbuzz")
+    expect(fizzbuzz.play(15)).to eq("Fizzbuzz")
   end
 
   it "fizzbuzzes for 30" do
-    result = fizzbuzz.play(30)
-    expect(result).to eq("Fizzbuzz")
+    expect(fizzbuzz.play(30)).to eq("Fizzbuzz")
   end
 
   it "returns the number otherwise" do
-    result = fizzbuzz.play(1)
-    expect(result).to eq(1)
+    expect(fizzbuzz.play(1)).to eq(1)
   end
 end
