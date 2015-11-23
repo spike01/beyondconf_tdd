0a90c82eed25d64b4d8ee8236fe160e98577453f
Spike <spike01@gmail.com>
2015-11-23 20:27:13 +0000

    REFACTOR - rename other two branches

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,9 +3,9 @@ class Fizzbuzz
   def play(number)
     if is_divisible_by_15?(number)
       "Fizzbuzz"
-    elsif number % 5 == 0
+    elsif is_divisible_by_5?(number)
       "Buzz"
-    elsif number % 3 == 0
+    elsif is_divisible_by_3?(number)
       "Fizz"
     else
       number
@@ -14,5 +14,13 @@ class Fizzbuzz
 
   def is_divisible_by_15?(number)
     number % 15 == 0
+  end
+
+  def is_divisible_by_5?(number)
+    number % 5 == 0
+  end
+
+  def is_divisible_by_3?(number)
+    number % 3 == 0
   end
 end
